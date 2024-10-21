6c72ffd66efa88c23b78740e670001c058f778aa
심은주 <nyaknya@naver.com>
2024-10-22 03:35:33 +0900

    ✨ feat: 데이터 fetch 유틸함수 추가

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -1,6 +1,8 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 import Button from '../Button';
+import apiRequest from '../../../utils/apiRequest';
+import { useEffect, useState } from 'react';
 
 const cn = classNames.bind(styles);
 
@@ -9,11 +11,23 @@ interface HedaerProps {
 }
 
 export default function Header({ isStiky = true }: HedaerProps) {
+  const [userProfile, setUserProfile] = useState(null);
+  const fetchUserData = async () => {
+    const data = await apiRequest({ endpoint: '/api/sample/user' });
+    setUserProfile(data);
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  });
+
+  // const { email, name, profileImageSource } = userProfile;
+
   return (
     <header>
       <div className={cn('container')}>
         <img src="/images/logo.svg" alt="로고" />
-        <Button>로그인</Button>
+        {userProfile ? <span>있어여</span> : <Button>로그인</Button>}
       </div>
     </header>
   );
