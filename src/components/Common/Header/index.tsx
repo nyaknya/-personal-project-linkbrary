f6b573ab0fcbdd6ec5402bdbc71c61aad01c021d
심은주 <nyaknya@naver.com>
2024-10-22 01:33:48 +0900

    ✨ feat: 세팅 수정 및 헤더 추가

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -1,1 +1,16 @@
 import styles from './Header.module.scss';
+import classNames from 'classnames';
+
+const cn = classNames.bind(styles);
+
+interface HedaerProps {
+  isStiky?: boolean;
+}
+
+export default function Header({ isStiky = true }: HedaerProps) {
+  return (
+    <header>
+      <div className={cn('container')}>테스트</div>
+    </header>
+  );
+}
