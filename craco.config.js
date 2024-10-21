d6b817a693bac28858b8fd1b207080223990fec1
심은주 <nyaknya@naver.com>
2024-10-22 03:11:42 +0900

    ✨feat: Button 컴포넌트 추가

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   style: {
+    css: {
+      modules: true, // CSS Modules 활성화
+    },
     sass: {
       loaderOptions: {
         additionalData: `
--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -1,5 +1,6 @@
 import styles from './Header.module.scss';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
+import Button from '../Button';
 
 const cn = classNames.bind(styles);
 
@@ -12,6 +13,7 @@ export default function Header({ isStiky = true }: HedaerProps) {
     <header>
       <div className={cn('container')}>
         <img src="/images/logo.svg" alt="로고" />
+        <Button>로그인</Button>
       </div>
     </header>
   );
