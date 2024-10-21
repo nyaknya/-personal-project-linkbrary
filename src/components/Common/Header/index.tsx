5a76adf0f18334ee9e196708ff039d732c0b0c4e
심은주 <nyaknya@naver.com>
2024-10-22 04:01:38 +0900

    ✨ feat: 헤더 완성

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -2,15 +2,12 @@ import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 import Button from '../Button';
 import apiRequest from '../../../utils/apiRequest';
+import UserProfile from './UserProfile';
 import { useEffect, useState } from 'react';
 
 const cn = classNames.bind(styles);
 
-interface HedaerProps {
-  isStiky?: boolean;
-}
-
-export default function Header({ isStiky = true }: HedaerProps) {
+export default function Header() {
   const [userProfile, setUserProfile] = useState(null);
   const fetchUserData = async () => {
     const data = await apiRequest({ endpoint: '/api/sample/user' });
@@ -21,13 +18,24 @@ export default function Header({ isStiky = true }: HedaerProps) {
     fetchUserData();
   });
 
-  // const { email, name, profileImageSource } = userProfile;
+  if (!userProfile) {
+    return <span>Loading...</span>;
+  }
+  const { name, email, profileImageSource } = userProfile;
 
   return (
     <header>
       <div className={cn('container')}>
         <img src="/images/logo.svg" alt="로고" />
-        {userProfile ? <span>있어여</span> : <Button>로그인</Button>}
+        {userProfile ? (
+          <UserProfile
+            name={name}
+            email={email}
+            profileImageSource={profileImageSource}
+          />
+        ) : (
+          <Button>로그인</Button>
+        )}
       </div>
     </header>
   );
