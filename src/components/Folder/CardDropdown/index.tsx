39e18e036b6a022a2591a6bd5aa7530247554699
심은주 <nyaknya@naver.com>
2024-11-03 22:35:28 +0900

    🐛 fix: 폴더 add 올바른 곳에 지정

--- src/components/Folder/CardDropdown/index.tsx
+++ src/components/Folder/CardDropdown/index.tsx
@@ -1,13 +1,22 @@
 import styles from './CardDropdown.module.scss';
 import classNames from 'classnames/bind';
+import useModal from '../../../hooks/useModal';
 const cn = classNames.bind(styles);
 
 export default function CardDropdown() {
+  const { openModal } = useModal();
+
+  const handleAddClick = (e: React.MouseEvent<HTMLLIElement>) => {
+    e.preventDefault();
+    openModal({ type: 'folderAdd' });
+  };
   return (
     <div className={cn('kebab-more-box')}>
       <ul>
         <li>삭제하기</li>
-        <li className={cn('add-folder')}>폴더에 추가</li>
+        <li className={cn('add-folder')} onClick={(e) => handleAddClick(e)}>
+          폴더에 추가
+        </li>
       </ul>
     </div>
   );
