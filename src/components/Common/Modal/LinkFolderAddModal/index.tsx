9625f9f2ba805f226f2e87a8f315565f647e3b17
심은주 <nyaknya@naver.com>
2024-11-04 00:15:04 +0900

    ✨ feat: 폴더에 추가 완성

--- src/components/Common/Modal/LinkFolderAddModal/index.tsx
+++ src/components/Common/Modal/LinkFolderAddModal/index.tsx
@@ -1,5 +1,12 @@
+import { useEffect, useState } from 'react';
 import Button from '../../Button';
 import Modal from '../Modal';
+import { FolderCategoryData } from '../../../../types';
+import apiRequest from '../../../../utils/apiRequest';
+import classNames from 'classnames/bind';
+import styles from '../Modal.module.scss';
+
+const cn = classNames.bind(styles);
 
 interface LinkFolderAddModalProps {
   onClose: () => void;
@@ -10,10 +17,51 @@ export default function LinkFolderAddModal({
   onClose,
   linkUrl,
 }: LinkFolderAddModalProps) {
+  const [folderListData, setFolderListData] = useState<
+    FolderCategoryData[] | null
+  >(null);
+  const [selectedFolderId, setSelectedFolderId] = useState<number | null>(null);
+
+  const fetchFolderListData = async () => {
+    try {
+      const data = await apiRequest({ endpoint: '/api/users/1/folders' });
+      setFolderListData(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFolderListData();
+  }, []);
+
+  const handleFolderClick = (folderId: number) => {
+    setSelectedFolderId(folderId);
+  };
+
   return (
     <Modal onClose={onClose} className="link-folder-add-modal">
       <h2>폴더에 추가</h2>
       <p>{linkUrl}</p>
+      <ul>
+        {folderListData?.map((listitem) => (
+          <li
+            key={listitem.id}
+            onClick={() => handleFolderClick(listitem.id)}
+            className={cn(selectedFolderId === listitem.id ? 'selected' : '')}
+          >
+            <div>
+              <h3>{listitem.name}</h3>
+              <span>{listitem.link.count}개 링크</span>
+            </div>
+            {selectedFolderId === listitem.id ? (
+              <img src="/images/check.svg" />
+            ) : (
+              ''
+            )}
+          </li>
+        ))}
+      </ul>
       <Button
         onClick={() => {
           console.log('추후추가');
--- src/store/useFolderStore.ts
+++ src/store/useFolderStore.ts
@@ -3,10 +3,8 @@ import { create } from 'zustand';
 interface FolderState {
   selectedCategory: string;
   selectedCategoryId: number | null;
-  selectedCategoryLinkCount: number | null;
   setSelectedCategory: (category: string) => void;
   setSelectedCategoryId: (id: number | null) => void;
-  setSelectedCategoryLinkCount: (count: number | null) => void;
 }
 
 const useFolderStore = create<FolderState>((set) => ({
@@ -16,8 +14,6 @@ const useFolderStore = create<FolderState>((set) => ({
   setSelectedCategory: (category: string) =>
     set({ selectedCategory: category }),
   setSelectedCategoryId: (id: number | null) => set({ selectedCategoryId: id }),
-  setSelectedCategoryLinkCount: (count: number | null) =>
-    set({ selectedCategoryLinkCount: count }),
 }));
 
 export default useFolderStore;
