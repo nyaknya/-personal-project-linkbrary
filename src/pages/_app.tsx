8651c88c8a52a5800cf905b16190dd907bdc86e2
심은주 <nyaknya@naver.com>
2024-12-07 13:02:23 +0900

    ✨ feat: 리액트 쿼리 사전세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useEffect } from "react";
 
 import ModalContainer from "@/components/Common/Modal/ModalContainer";
@@ -6,6 +8,8 @@ import useUserStore from "@/store/useUserStore";
 import apiRequest from "@/utils/apiRequest";
 
 import type { AppProps } from "next/app";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   const { setUserId } = useUserStore();
@@ -34,9 +38,10 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [setUserId]);
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
       <ModalContainer />
-    </>
+      {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
+    </QueryClientProvider>
   );
 }
