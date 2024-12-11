535b0db8deb9c4ced79fdaaf0745b322574b9789
심은주 <nyaknya@naver.com>
2024-12-11 17:54:15 +0900

    ✨ feat: 즐겨찾기 기능 추가

--- src/components/Folder/Card/index.tsx
+++ src/components/Folder/Card/index.tsx
@@ -1,3 +1,5 @@
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import classNames from "classnames/bind";
 import Image from "next/image";
 import Link from "next/link";
@@ -5,6 +7,7 @@ import { useRef, useState } from "react";
 
 import useOutSideClick from "@/hooks/useOutSideClick";
 import { FolderLinksType } from "@/types";
+import apiRequest from "@/utils/apiRequest";
 import getElapsedTime from "@/utils/getElapsedTime";
 import sliceDate from "@/utils/sliceDate";
 
@@ -21,17 +24,21 @@ interface CardProps {
 
 export default function Card({ link, isOpen, onToggleDropdown }: CardProps) {
   const dropdownRef = useRef<HTMLDivElement>(null);
-  const { id, url, image_source, title, description, created_at } = link;
+  const [isFavorite, setIsFavorite] = useState(link.favorite); // 초기값 설정
 
-  const [currentSrc, setCurrentSrc] = useState(
-    image_source?.trim() || "/images/defaultimg.png"
-  );
-
+  const { url, id, image_source, title, description, created_at } = link;
   const postDate = sliceDate(created_at);
   const getTimeAgo = getElapsedTime(created_at);
 
-  const handleImageError = () => {
-    setCurrentSrc("/images/defaultimg.png");
+  const handleSrc =
+    image_source && image_source.trim() !== ""
+      ? image_source
+      : "/images/defaultimg.png";
+
+  const handleImageError = (
+    event: React.SyntheticEvent<HTMLImageElement, Event>
+  ) => {
+    event.currentTarget.src = "/images/defaultimg.png";
   };
 
   useOutSideClick({
@@ -41,28 +48,58 @@ export default function Card({ link, isOpen, onToggleDropdown }: CardProps) {
     },
   });
 
+  const toggleFavoriteMutation = useMutation({
+    mutationFn: async () => {
+      return await apiRequest({
+        endpoint: `/links/${id}`,
+        method: "PUT",
+        body: { favorite: !isFavorite },
+      });
+    },
+    onSuccess: () => {
+      setIsFavorite((prev) => !prev);
+    },
+    onError: (error: AxiosError) => {
+      if (error.response?.status === 400) {
+        alert("요청 오류: 입력값을 확인해주세요.");
+      } else if (error.response?.status === 401) {
+        alert("인증 오류: 다시 로그인해주세요.");
+      } else if (error.response?.status === 403) {
+        alert("권한 오류: 이 작업을 수행할 권한이 없습니다.");
+      } else {
+        alert("즐겨찾기 설정 중 오류가 발생했습니다.");
+      }
+    },
+  });
+
+  const handleFavoriteClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+    toggleFavoriteMutation.mutate();
+  };
+
   return (
-    <Link href={url} className={cn("card")} target="_blank">
+    <Link href={url} className={cn("card")} target="blank">
       {isOpen && (
         <div ref={dropdownRef}>
           <CardDropdown id={id} url={url} />
         </div>
       )}
       <Image
-        src="/images/star.svg"
+        src={isFavorite ? "/images/star_filled.svg" : "/images/star.svg"}
         alt="즐겨찾기"
         className={cn("bookmark-icon")}
         width={34}
         height={34}
+        onClick={handleFavoriteClick}
       />
       <div className={cn("image-box")}>
         <Image
-          src={currentSrc}
-          alt={title || "Default Image"}
+          src={handleSrc}
+          onError={handleImageError}
+          alt={title}
           width={346}
           height={200}
-          onError={handleImageError}
-          priority
         />
       </div>
       <div className={cn("card-content")}>
